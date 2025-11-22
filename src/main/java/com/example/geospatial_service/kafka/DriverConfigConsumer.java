4112ea908ee5e72dea59c017a48603fcd0ca91ac
201912025 <michi2012@naver.com>
2025-11-22 14:57:04 +0900

    feat: 배차 취소시 웹소켓으로 변경 전송시간 주기 기능 구현

--- src/main/java/com/example/geospatial_service/kafka/DriverConfigConsumer.java
+++ src/main/java/com/example/geospatial_service/kafka/DriverConfigConsumer.java
@@ -1,6 +1,7 @@
 package com.example.geospatial_service.kafka;
 
 import com.example.geospatial_service.handler.LocationWebSocketHandler;
+import com.example.geospatial_service.kafka.dto.TripCanceledEvent;
 import com.example.geospatial_service.kafka.dto.TripCompletedEvent;
 import com.example.geospatial_service.kafka.dto.TripMatchedEvent;
 import com.example.geospatial_service.kafka.dto.socket.DriverConfigMessage;
@@ -27,6 +28,12 @@ public class DriverConfigConsumer {
     // 운행 종료 -> 한가해짐 -> 10초 주기
     @KafkaHandler
     public void handleTripCompleted(TripCompletedEvent event) {
+        locationWebSocketHandler.sendConfigToDriver(event.driverId(), DriverConfigMessage.lowFrequency());
+    }
+
+    @KafkaHandler
+    public void handleTripCanceled(TripCanceledEvent event) {
+        log.info("여정 취소 수신 -> 기사({}) 10초 주기로 변경", event.driverId());
         locationWebSocketHandler.sendConfigToDriver(event.driverId(), DriverConfigMessage.lowFrequency());
     }
 
